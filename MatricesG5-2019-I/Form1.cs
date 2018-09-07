4d96ad029f591669da407a5d1dfdb483c128960d
faraday58 <jarodriguezvera@gmail.com>
2018-09-07 14:42:49 -0500

    Excepciones en clase

--- MatricesG5-2019-I/Form1.cs
+++ MatricesG5-2019-I/Form1.cs
@@ -9,6 +9,11 @@ namespace MatricesG5_2019_I
         Unidimensional m1;
         Unidimensional m2;
         Unidimensional m3;
+
+        Multidimensional mu1;
+        Multidimensional mu2;
+        Multidimensional mu3;
+
         public Form1()
         {
             InitializeComponent();
@@ -16,16 +21,67 @@ namespace MatricesG5_2019_I
 
         private void btnEntrada_Click(object sender, EventArgs e)
         {
-            m1 = Unidimensional.Leer(txtbEntrada.Text);
-            lbMatriz1.Text = m1.ToString(); ;
+            try
+            {
+                if( txtbEntrada.Text.Contains(";")  )
+                {
+                    mu1 = Multidimensional.Leer(txtbEntrada.Text);
+                    lbMatriz1.Text = mu1.ToString();
+                }
+                else
+                {
+                    m1 = Unidimensional.Leer(txtbEntrada.Text);
+                    lbMatriz1.Text = m1.ToString();
+                }                
+                errorProv.Clear();
+            }
+            catch(FormatException error)
+            {
+                errorProv.SetError(txtbEntrada, error.Message);
+            }
+             
             txtbEntrada.Clear();
         }
 
         private void btnSumar_Click(object sender, EventArgs e)
         {
-            m2 = Unidimensional.Leer(txtbEntrada.Text);
-            m3 = m1 + m2;
-            lbMatriz2.Text = m3.ToString();
+            try
+            {
+                if (txtbEntrada.Text.Contains(";"))
+                {
+                    mu2 = Multidimensional.Leer(txtbEntrada.Text);
+                    if((mu1.N != mu2.N) || (mu1.M != mu2.M))
+                    {
+
+                        throw new ApplicationException("Error de dimensión");
+                    }
+                    mu3 = mu1 + mu2;
+                    lbMatriz2.Text = mu3.ToString();
+                    
+                }
+                else {
+                    m2 = Unidimensional.Leer(txtbEntrada.Text);
+                    if (m1.N != m2.N)
+                    {
+                        throw new ApplicationException("Error de dimesión!!!");
+
+                    }
+                    m3 = m1 + m2;
+                    lbMatriz2.Text = m3.ToString();
+                }
+                errorProv.Clear();
+                
+            }
+            catch(ApplicationException error)
+            {
+                errorProv.SetError(txtbEntrada, error.Message);
+            }
+            catch (FormatException error)
+            {
+
+                errorProv.SetError(txtbEntrada, error.Message);
+            }
+            
             txtbEntrada.Clear();
         }
     }
---
+++ MatricesG5-2019-I/Multidimensional.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+
+namespace MatricesG5_2019_I
+{
+    class Multidimensional:Matriz
+    {
+        private float[,] matriz;
+
+        public Multidimensional(byte M,byte N   )
+        {
+            this.M = M;
+            this.N = N;
+
+            matriz = new float[M, N];
+         }
+
+        public static Multidimensional Leer(string matriz )
+        {
+            //1 2 3;4 5 6;7 8 9
+            string[] renglones = matriz.Split(';');
+            string[] columnas = renglones[0].Split(' ');
+
+            Multidimensional m1 = new Multidimensional((byte)renglones.Length,(byte)columnas.Length);
+
+            for( int i=0; i < m1.M; i++)
+            {
+                columnas = renglones[i].Split(' ');
+                for (int j=0; j < m1.N; j ++)
+                {
+
+                    m1.matriz[i, j] = float.Parse(columnas[j]);
+                }
+                
+            }
+
+            return m1;
+        }
+
+        public static Multidimensional operator +(Multidimensional mu1, Multidimensional mu2)
+        {
+            Multidimensional mu3 = new Multidimensional(mu1.M, mu1.N);
+
+            for(int i = 0; i < mu3.M; i++  )
+            {
+                for(int j=0; j< mu3.N; j++ )
+                {
+                    mu3.matriz[i, j] = mu1.matriz[i, j] + mu2.matriz[i, j];
+                }
+            }
+            return mu3;
+        }
+
+
+        public override string ToString()
+        {
+            //1 2 3
+            //4 5 6
+            //7 8 9
+            string aux = "";
+            for (int i=0; i < M; i ++)
+            {
+                for(int j=0; j  < N; j++)
+                {
+                    aux = aux + " " + matriz[i, j];
+                }
+                aux = aux + "\n";
+            }
+
+            return aux;
+        }
+
+    }
+}
